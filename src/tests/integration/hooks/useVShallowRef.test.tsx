78586573d63e8e3cc6ca1fd60e3ec0b9b37ac586
Omer M <omerplays1411@gmail.com>
2023-10-08 11:25:48 +0300

    added new test

--- src/tests/integration/hooks/useVShallowRef.test.tsx
+++ src/tests/integration/hooks/useVShallowRef.test.tsx
@@ -2,7 +2,7 @@ import { renderHook, render, fireEvent, act } from '@testing-library/react';
 import useVRef from '../../../hooks/useVShallowRef';
 import { describe, expect, it } from 'vitest';
 
-describe('useVRef integration test', () => {
+describe('useShallowVRef integration test', () => {
   it('could be passed as props', () => {
     const { result } = renderHook(useVRef, {
       initialProps: 1,
@@ -21,32 +21,32 @@ describe('useVRef integration test', () => {
     expect(button.innerHTML).toBe('1');
   });
 
-  // it(`is reactive when passed as props (outside value change => component' value change)`, () => {
-  //   const { result } = renderHook(useVRef, {
-  //     initialProps: 1,
-  //   });
+  it(`is not reactive when passed as props (outside value change => component's value change)`, () => {
+    const { result } = renderHook(useVRef, {
+      initialProps: 1,
+    });
 
-  //   const component = ({
-  //     value,
-  //   }: {
-  //     value: ReturnType<typeof useVRef<number>>;
-  //   }) => {
-  //     return <button>{value.__intenalValue}</button>;
-  //   };
+    const component = ({
+      value,
+    }: {
+      value: ReturnType<typeof useVRef<number>>;
+    }) => {
+      return <button>{value.value}</button>;
+    };
 
-  //   const { getByRole } = render(
-  //     component({ value: result.current })
-  //   );
+    const { getByRole } = render(
+      component({ value: result.current })
+    );
 
-  //   act(() => {
-  //     result.current.value = 2;
-  //   });
+    act(() => {
+      result.current.value = 2;
+    });
 
-  //   const button = getByRole('button');
-  //   expect(button.innerHTML).toBe('2');
-  // });
+    const button = getByRole('button');
+    expect(button.innerHTML).toBe('1');
+  });
 
-  it(`is reactive when passed as props (component' value change => outside value change)`, () => {
+  it(`is reactive when passed as props (component's value change => outside value change)`, () => {
     const { result } = renderHook(useVRef<number>, { initialProps: 1 });
 
     const component = ({
